75bc404ebd580e59414b17a1cd030116130d5123
jiakai <jia.kai66@gmail.com>
2014-11-20 22:24:30 +0800

    orientation smoothing

---
+++ riesz/fastmath.pyx
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# $File: fastmath.pyx
+# $Date: Thu Nov 20 21:33:07 2014 +0800
+# $Author: jiakai <jia.kai66@gmail.com>
+
+import numpy as np
+cimport numpy as np
+cimport cython
+
+ctypedef np.float32_t DTYPE_t
+cdef DTYPE_t MAX_DIFF = np.pi / 2 + 1e-4
+
+cdef _update_row(np.ndarray[DTYPE_t, ndim=2] row, DTYPE_t tgt_val):
+    cdef DTYPE_t mean, mean_orig
+    mean = np.mean(row[:, 1])
+    if abs(mean - tgt_val) <= MAX_DIFF:
+        return
+    mean_orig = mean
+    cdef int chg_sign = 0
+    while abs(mean - tgt_val) > MAX_DIFF:
+        if mean > tgt_val:
+            mean -= np.pi
+        else:
+            mean += np.pi
+        chg_sign ^= 1
+    row[:, 1] += mean - mean_orig
+    if chg_sign:
+        np.negative(row[:, 2], row[:, 2])
+
+@cython.boundscheck(False)
+def smooth_orient(np.ndarray[DTYPE_t, ndim=3] riesz):
+    cdef int row, col
+    cdef int nr_row = riesz.shape[0]
+    cdef int nr_col = riesz.shape[1]
+    cdef DTYPE_t vprev, vnow
+
+    # independently smooth each row
+    for row in range(nr_row):
+        vprev = riesz[row, 0, 1]
+        for col in range(1, nr_col):
+            vnow = riesz[row, col, 1]
+            if abs(vnow - vprev) > MAX_DIFF:
+                phase = riesz[row, col, 2]
+                while abs(vnow - vprev) > MAX_DIFF:
+                    if vnow < vprev:
+                        vnow += np.pi
+                    else:
+                        vnow -= np.pi
+                    phase = -phase
+                riesz[row, col, 1] = vnow
+                riesz[row, col, 2] = phase
+        _update_row(riesz[row], 0)
+
+    vprev = np.mean(riesz[0, :, 1])
+    for row in range(nr_row):
+        _update_row(riesz[row], vprev)
+        vprev = np.mean(riesz[row, :, 1])
--- riesz/riesz.py
+++ riesz/riesz.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 # $File: riesz.py
-# $Date: Thu Nov 20 00:12:30 2014 +0800
+# $Date: Thu Nov 20 22:23:16 2014 +0800
 # $Author: jiakai <jia.kai66@gmail.com>
+
+import pyximport
+pyximport.install()
 
 import cv2
 import numpy as np
 import sys
 from scipy import signal
+from fastmath import smooth_orient
 
 EPS = 1e-10
 
@@ -20,6 +24,9 @@ def build_lap_pyr(img, min_size=10, max_nr_level=None):
     """:return: list of images, layers in the Laplacian Pyramid"""
     if max_nr_level is None:
         max_nr_level = float('inf')
+    im_min = np.min(img)
+    im_max = np.max(img)
+    assert im_min >= 0 and im_max <= 255 and im_max >= 10, (im_min, im_max)
     img = img.astype('float32') / 255.0
     rst = []
     while min(img.shape[:2]) >= min_size and len(rst) < max_nr_level:
@@ -36,18 +43,36 @@ def get_riesz_triple(img):
     """
     assert img.ndim == 2
     img = img.astype('float32')
-    r1 = signal.convolve2d(img, RIESZ_KERNEL, mode='same')
-    r2 = signal.convolve2d(img, RIESZ_KERNEL.T, mode='same')
+    r1 = signal.convolve2d(img, RIESZ_KERNEL, mode='valid')
+    r2 = signal.convolve2d(img, RIESZ_KERNEL.T, mode='valid')
+    kh, kw = RIESZ_KERNEL.shape
+    img = img[kh/2:-(kh/2), kw/2:-(kw/2)]
     amp = np.sqrt(np.square(img) + np.square(r1) + np.square(r2)) + EPS
     phase = np.arccos(img / amp)
     t = amp * np.sin(phase) + EPS
     orient = np.arctan2(r2 / t, r1 / t)
-    return np.concatenate(map(lambda a: np.expand_dims(a, axis=2),
-                              (amp, orient, phase)), axis=2)
+    rst = np.concatenate(map(lambda a: np.expand_dims(a, axis=2),
+                             (amp, orient, phase)), axis=2)
+    assert np.all(np.isfinite(rst)), \
+        np.transpose(np.nonzero(1 - np.isfinite(rst)))
+    smooth_orient(rst)
+    return rst
 
 def get_phase_diff(riesz0, riesz1):
-    oriet_sign = (np.sign(riesz0[:, :, 1]) + np.sign(riesz1[:, :, 1])) / 2
-    return (riesz1[:, :, 2] - riesz0[:, :, 2]) * oriet_sign
+    assert riesz0.shape == riesz1.shape
+    if riesz0.ndim == 2:
+        assert riesz0.shape[1] == 3
+        rst = riesz1[:, 2] - riesz0[:, 2]
+    else:
+        rst = riesz1[:, :, 2] - riesz0[:, :, 2]
+    rst = np.mod(rst, np.pi * 2)
+    rst[rst >= np.pi] -= np.pi * 2
+    return rst
+
+def get_avg_phase_diff(riesz0, riesz1):
+    amp = (riesz0[:, :, 0] + riesz1[:, :, 0]) / 2
+    pd = get_phase_diff(riesz0, riesz1)
+    return np.sum(pd * amp) / np.sum(amp)
 
 def normalize_disp(img):
     img = cv2.normalize(img, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
@@ -60,17 +85,45 @@ def imshow(name, img, wait=False):
             sys.exit()
 
 def test_motion():
+    import matplotlib.pyplot as plt
     SIZE = 500
     k = np.pi / 40
     shift = 0.05 * k
     x0 = np.arange(SIZE) * k
-    make = lambda x: np.tile(np.sin(x), SIZE).reshape(SIZE, SIZE)
+    def make(x):
+        x = np.tile(x, SIZE).reshape(SIZE, SIZE)
+        y = np.tile(x0, SIZE).reshape(SIZE, SIZE).T
+        val = (np.sin(x + y) + 1) / 2
+        return val * 255
+        return normalize_disp(val)
     img0 = make(x0)
     img1 = make(x0 + shift)
     riesz0 = get_riesz_triple(build_lap_pyr(img0, max_nr_level=1)[0])
     riesz1 = get_riesz_triple(build_lap_pyr(img1, max_nr_level=1)[0])
+    if True:
+        row = riesz0[30]
+        row1 = riesz1[30]
+        plt.figure()
+        plt.subplot(4, 1, 1)
+        plt.title('amp')
+        plt.plot(row[:, 0])
+        plt.subplot(4, 1, 2)
+        plt.title('orientation')
+        plt.plot(row[:, 1])
+        plt.subplot(4, 1, 3)
+        plt.title('phase')
+        plt.plot(row[:, 2])
+        plt.subplot(4, 1, 4)
+        plt.title('phase_diff')
+        plt.plot(get_phase_diff(row, row1))
+        plt.figure()
+        plt.title('row_orient_avg')
+        plt.plot(np.mean(riesz0[:, :, 1], axis=1))
+        plt.show()
     riesz_diff = riesz0 - riesz1
-    print shift, np.mean(get_phase_diff(riesz0, riesz1))
+    print riesz0[100, 30:70]
+    print riesz_diff[100, 30:70]
+    print shift, get_avg_phase_diff(riesz0, riesz1)
 
     imshow('img0', img0)
     imshow('img1', img1)
